feab32eff433060ea8f16255054aa4a1169ce352
Nicole Hernandez <nh061361@gmail.com>
2025-12-02 07:58:51 +0000

    starting build "d6199e7f-18ae-41b3-af4f-b18d934ab070" FETCHSOURCE From h

--- next.config.ts
+++ next.config.ts
@@ -31,7 +31,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'httpshttps',
+        protocol: 'https',
         hostname: 'firebasestorage.googleapis.com',
         port: '',
         pathname: '/**',
