5e332ad71d9e8f45f93e13b0368578fa25671c88
Nicole Hernandez <nh061361@gmail.com>
2025-12-04 19:36:15 +0000

    super, ahora otra cosa cuando vusco en la web vestalar sale simpre este

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,9 @@ export const metadata: Metadata = {
   title: 'Vestalar | Reformas Integrales en Zaragoza y toda España',
   description: 'Especialistas en reformas integrales de cocinas, baños y viviendas completas. Presupuestos a medida en Zaragoza y resto de España. Calidad y confianza garantizadas.',
   keywords: 'reformas, reformas integrales, reformas zaragoza, reformas españa, presupuesto reforma, reformar cocina, reformar baño, empresa de reformas, vestalar',
+  icons: {
+    icon: 'https://firebasestorage.googleapis.com/v0/b/vestalarservicios.firebasestorage.app/o/Herramientas%2Flogo%2FVestalar_Imagotipo_original_cmyk.png?alt=media&token=4d56df93-01bf-44d9-926c-dfc261a9a62e',
+  },
 };
 
 export default function RootLayout({
