d131fcb5052be05f714ed5492c4da89171c472cb
Nicole Hernandez <nh061361@gmail.com>
2025-12-08 20:15:17 +0000

    estoy muy preocuapada a mi me sigue pasando lo mismo <Firebase Studio> ✘

---
+++ next.config.js
@@ -0,0 +1,63 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  output: 'standalone',
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  async headers() {
+    return [
+      {
+        source: '/_next/:path*',
+        headers: [
+          {key: 'Access-Control-Allow-Origin', value: '*'},
+          {key: 'Access-Control-Allow-Methods', value: 'GET, OPTIONS, HEAD'},
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'Next-Action, Next-Router-State-Tree, Next-Router-Prefetch, RSC',
+          },
+        ],
+      },
+    ];
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'png.pngtree.com',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+};
+
+module.exports = nextConfig;
